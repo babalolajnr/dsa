23728541a48bbbfe1f1fc5393b006429283d97bf
Abdulqudduus Babalola <babalolajnr@gmail.com>
2021-07-15 16:12:46 +0100

    add remove method

--- linked_list.py
+++ linked_list.py
@@ -91,6 +91,28 @@ class LinkedList:
             prev_node.next_node = new
             new.next_node = next_node
 
+    def remove(self, key):
+        """
+        Removes  node containnig data that matches the key
+        Returns the node or None if the key doesn't exist
+        Takes O(n) time
+        """
+        current = self.head
+        previous = None
+        found = False
+
+        while current and not found:
+            if current.data == key and current is self.head:
+                found = True
+                self.head = current.next_node
+            elif current.data == key:
+                found = True
+                previous.next_node = current.next_node
+            else:
+                previous = current
+                current = current.next_node
+        return current
+
     def __repr__(self) -> str:
         """
         Returns a string representation of the list
